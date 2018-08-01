d643eeaa5d7343bad9b6335241a86607ca7f9561
AndiAn94 <andreas_anisimov@web.de>
2018-08-01 17:01:15 +0200

    added server which receives message, parse it and send a response back, added common which can be used by both client and server

---
+++ libdoipserver/include/DoIPServer.h
@@ -0,0 +1,31 @@
+#ifndef DOIPSERVER_H
+#define DOIPSERVER_H
+
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstddef>
+#include "DoIPGenericHeaderHandler.h"
+#include "RoutingActivationHandler.h"
+
+const int _ServerPort = 13400;
+const int _MaxDataSize = 64;
+
+class DoIPServer {
+
+    public:
+        unsigned char data[_MaxDataSize];
+        int sockfd_receiver, sockfd_sender;
+        struct sockaddr_in serverAdress;
+        
+        void setupSocket();
+        void receiveMessage();
+        void sendMessage(unsigned char* message, int messageLenght);
+        void closeSocket();
+};
+
+
+#endif /* DOIPSERVER_H */
+
