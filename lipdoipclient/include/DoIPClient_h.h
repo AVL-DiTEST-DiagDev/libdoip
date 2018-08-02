371ef90ffb7829373ec9127913460d4eb6c62587
baris <baris.delibas@hotmail.de>
2018-08-02 12:59:37 -0700

    added client which build a request, send to the server and receives a response, added unit test to check connection

---
+++ lipdoipclient/include/DoIPClient_h.h
@@ -0,0 +1,36 @@
+
+#ifndef DOIPCLIENT_H
+#define DOIPCLIENT_H
+#include <arpa/inet.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <cstddef>
+#include <stdlib.h>
+#include <cstdlib>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+const int _serverPortNr=13400;
+const int _maxDataSize=64;
+
+class DoIPClient{
+
+    unsigned char _receivedData[_maxDataSize];
+    int _sockFd;
+    int _connected;
+    struct sockaddr_in _serverAddr; 
+    
+    private:
+       const pair<int,unsigned char*>* buildRoutingActivationRequest();
+    public:
+        void startTcpConnection();     
+        void sendRoutingActivationRequest();
+        void receiveRoutingActivationResponse();
+        void closeTcpConnection();
+};
+
+
+#endif /* DOIPCLIENT_H */
+
