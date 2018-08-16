c2303ccd3249c8e5d273549129980b658f2bdb73
LinuxVM <gerrit.riesch@live.de>
2018-08-16 17:20:58 +0200

    VehicleIdentificationHandler Implementation#2

--- libdoipserver/include/VehicleIdentificationHandler.h
+++ libdoipserver/include/VehicleIdentificationHandler.h
@@ -1,1 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   VehicleIdentificationHandler.h
+ * Author: linuxvm
+ *
+ * Created on 13. August 2018, 16:51
+ */
+
+#ifndef VEHICLEIDENTIFICATIONHANDLER_H
+#define VEHICLEIDENTIFICATIONHANDLER_H
+
+
+#include "DoIPGenericHeaderHandler.h"
+
+
+unsigned char* createVehicleIdentificationResponse(char* VIN,unsigned char* LogicalAdress,unsigned char* EID,unsigned char* GID,unsigned char FurtherActionReq);
+
+const int _VIResponseLength = 32;
+
+#endif /* VEHICLEIDENTIFICATIONHANDLER_H */
 
