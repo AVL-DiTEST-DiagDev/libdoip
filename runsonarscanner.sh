deb75a630f7b51d0cf04bd6866894129bb4a0154
Tobias Jaehnel <tjaehnel@gmail.com>
2020-07-31 08:06:34 +0200

    Added sonar branch analysis

--- runsonarscanner.sh
+++ runsonarscanner.sh
@@ -5,7 +5,13 @@ then
 	exit 1
 fi
 
+SONAR_BRANCH=""
+if [ $# -gt 1 ]
+then
+	SONAR_BRANCH="-Dsonar.branch.name=$2"
+fi
+
 docker build -t libdoipbuild .
 docker run --rm -v "$(pwd):/proj" -w /proj libdoipbuild build-wrapper-linux-x86-64 --out-dir bw-output make clean all
-docker run --rm -v "$(pwd):/proj" -w /proj libdoipbuild sonar-scanner -Dsonar.host.url="https://sonarcloud.io" -Dsonar.login="$1"
+docker run --rm -v "$(pwd):/proj" -w /proj libdoipbuild sonar-scanner -Dsonar.login="$1" $SONAR_BRANCH
 
